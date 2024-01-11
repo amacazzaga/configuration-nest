04555265eb51d4360e8bd844213fd507a7000748
amacazzagacodice <agustin.macazzaga@codice.dev>
2024-01-11 12:05:07 -0300

    AppserviceWithConfig example

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,3 +9,22 @@ export class AppService {
     return 'Hello World!';
   }
 }
+// esto deberia estar en un archivo como api-config.service.ts
+@Injectable() 
+export class ApiConfigService {
+  constructor(private configService: ConfigService) {}
+  get isAuthEnabled(): boolean {
+    // Devuelve true si el valor de la configuración 'AUTH_ENABLED' es 'true' , esto es una configuracion que puede utilizarse en algun otro servicio
+    // la idea es usarlo para saber si auht_enables es true or false y luego en consecuencia de ello 
+    return this.configService.get('AUTH_ENABLED') === 'true';
+  }
+}
+// y podria usarse en un app.service que se viera asi:
+@Injectable()
+export class AppServiceWithConfig {
+  constructor(apiConfigService: ApiConfigService) {
+    if (apiConfigService.isAuthEnabled) {
+      // Autenticación está habilitada o no?
+    }
+  }
+}
