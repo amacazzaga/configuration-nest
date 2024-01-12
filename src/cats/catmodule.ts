bc2771607e840816f56e71b6e24a5f871c5812ee
amacazzagacodice <agustin.macazzaga@codice.dev>
2024-01-12 17:16:26 -0300

    indentation

--- src/cats/catmodule.ts
+++ src/cats/catmodule.ts
@@ -8,6 +8,7 @@ import { Cat, CatSchemaAsDecorator } from './cat.schema';
   imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchemaAsDecorator }])],//la necesidad de extraer el nombre de la clase con .name está relacionada con la forma en que 
   //Mongoose asigna nombres a los modelos. 
   //Cuando se define un modelo en Mongoose, este necesita un nombre único que generalmente se deriva del nombre de la clase del modelo.
+  // Mongoose tambien aporta middleware para hacer validaciones, por ejemplo antes de que se guarde algo en la base de datos!
   //controllers: [CatsController],
   //providers: [CatsService],
 })
