e367256fab8b8e8f3632b61d8520cf271b568f77
amacazzagacodice <agustin.macazzaga@codice.dev>
2024-01-16 10:01:03 -0300

    Jwt on service

---
+++ src/authentication/authentication.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './authentication.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login') // recordemos que recuperando el servicio desde nuestro controller, el endpoint de sign in sera login como se indica
+  signIn(@Body() signInDto: Record<string, any>) {
+    return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+}
--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,12 +1,15 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 
 // Este servicio se ocupara de recuperar usuario y contraseña y si esta todo OK, devolvera un JWT
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService,
+    private jwtService:JwtService
+    ) {}
 
   async signIn(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne(username);
@@ -15,6 +18,9 @@ export class AuthService {
     }
     const { password, ...result } = user;
   // aca deberiamos entregar un Jason Web Token como respuesta a un usuario con credenciales validas para iniciar sesion
-    return result;
+  const payload = { sub: user.userId, username: user.username };
+    return {
+      access_token: await this.jwtService.signAsync(payload), // aca devolvemos ese JWT , que toma como parametro el user id y el user name para armarlo
+    };
   }
 }
